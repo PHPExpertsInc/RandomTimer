709f8ba50bf7ca5470f1c70e09bfcaee40f0b9d6
Theodore R. Smith <theodore@phpexperts.pro>
2021-12-12 16:00:37 -0600

    Use sane defaults.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-
+rm -r ~/.androidjs/; time androidjs build --release
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /home/tsmith/phpexperts.keystore "dist/Random Timer.apk" phpexperts
 zipalign -v 4 "dist/Random Timer.apk" RandomTimer.apk
